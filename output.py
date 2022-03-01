40e79c3d11b02c39117a71d5ffe7cb12860669d2
David Dickinson <36966460+rnddave@users.noreply.github.com>
2022-03-01 15:09:43 +0000

    Delete output.py

--- output.py
+++
@@ -1,8 +0,0 @@
-'''
-somehow need to pass the dictionary from inputs to here? 
-
-the team with the fewer wins needs to play at home
-
-the team with fewer wins plays team with most wins
--- team with second fewest wins plays team with second most wins and so on
-''' 
